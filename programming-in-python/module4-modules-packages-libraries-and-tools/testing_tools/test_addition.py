0bdcfd986a06ece9e02c7f05b7e06578915d4185
cjjohanson <46818843+cjjohanson@users.noreply.github.com>
2024-06-24 13:30:04 -0400

    wrap up module 4

--- programming-in-python/module4-modules-packages-libraries-and-tools/testing_tools/test_addition.py
+++ programming-in-python/module4-modules-packages-libraries-and-tools/testing_tools/test_addition.py
@@ -1,3 +1,13 @@
 import pytest
 import addition
 
+def test_add():
+    assert addition.add(4, 5) == 9
+
+
+def test_subtract():
+    assert addition.sub(4, 5) == -1
+
+
+# Use python -m pytest test_addition.py to run the tests
+# and see the outputs
